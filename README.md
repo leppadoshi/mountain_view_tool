756707ff62fe2d7519ef50e97ea8a2382c0a2c11
leppadoshi <3402009-gdoshi@users.noreply.gitlab.com>
2019-12-18 11:56:56 -0800

    Forgot to save renderer.rb

--- README.md
+++ README.md
@@ -20,7 +20,9 @@ Or install it yourself as:
 
 ## Usage
 
-TODO: Write usage instructions here
+```ruby
+MountainViewTool::Renderer.copyright 'Hidden Mountain', 'All rights reserved'
+```
 
 ## Development
 
--- lib/renderer.rb
+++ lib/renderer.rb
@@ -1,4 +1,8 @@
 module MountainViewTool
   class Error < StandardError; end
-  # Your code goes here...
+  class Renderer
+		def self.copyright name, msg
+			"&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+		end
+	end
 end
